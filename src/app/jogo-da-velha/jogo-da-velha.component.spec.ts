73ff553c6d94ca21a5b761c4cea39dd520fef59b
Douglas Carlos <dnevescarlos@gmail.com>
2018-01-12 02:10:56 -0200

    ajustando teste

--- src/app/jogo-da-velha/jogo-da-velha.component.spec.ts
+++ src/app/jogo-da-velha/jogo-da-velha.component.spec.ts
@@ -1,14 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { JogoDaVelhaComponent } from './jogo-da-velha.component';
-
+import { JogoDaVelhaService } from './shared';
 describe('JogoDaVelhaComponent', () => {
   let component: JogoDaVelhaComponent;
   let fixture: ComponentFixture<JogoDaVelhaComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ JogoDaVelhaComponent ]
+      declarations: [ 
+        JogoDaVelhaComponent
+      ],
+      providers: [
+        JogoDaVelhaService
+      ]
     })
     .compileComponents();
   }));
